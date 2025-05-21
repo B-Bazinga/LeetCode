444d77f87680ee9bb75d95e3fe70460858e38d9a
Rohan Verma <whoami.rohanverma@gmail.com>
2025-05-21 11:25:54 +0530

    Time: 53 ms (5.12%), Space: 18 MB (48.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,9 +4,11 @@ class Solution:
         if len(s) != len(t):
             return False
         # Count the frequency for each character 
+        
+        # Count of each character, intially set it to zero
+        count_char = [0]*26  
 
-        count_char = [0]*26 # Count of each character 
-
+        
         for i in range (len(s)):
             count_char[ord(s[i])-ord('a')]+=1
             count_char[ord(t[i])-ord('a')]-=1
