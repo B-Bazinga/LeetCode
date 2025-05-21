5ab3d9a6303d5c5999b5367ad20e5f4556b0fab7
Rohan Verma <whoami.rohanverma@gmail.com>
2025-05-21 11:49:09 +0530

    Time: 15 ms (42.67%), Space: 18 MB (32.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        sum_s, sum_t = 0,0
         if len(s) != len(t):
             return False
-        # Count the frequency for each character 
-        
-        # Count of each character, intially set it to zero
-        count_char = [0]*26  
+        count_map = {}
 
-        
-        for i in range (len(s)):
-            count_char[ord(s[i])-ord('a')]+=1
-            count_char[ord(t[i])-ord('a')]-=1
-        
-        return all(c==0 for c in count_char)
+        for i in range(len(s)):
+            count_map[s[i]] = count_map.get(s[i],0) + 1
+            count_map[t[i]] = count_map.get(t[i],0) - 1
+
+        return all(c==0 for c in count_map.values())
